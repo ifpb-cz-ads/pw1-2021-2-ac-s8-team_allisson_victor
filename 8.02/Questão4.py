1a98bc14c1e541da7edae17a21c22109b187b093
VictorBrasil <48683006+VictorBrasil@users.noreply.github.com>
2022-02-02 14:13:54 -0300

    Upando questões pares

---
+++ 8.02/Questão4.py
@@ -0,0 +1,6 @@
+def area_triangulo(b, h):
+    return (b * h) / 2
+
+
+print("area_triângulo(6, 9) == 27 -> obtido:", area_triangulo(6,9))
+print("area_triângulo(5, 8) == 20 -> obtido:", area_triangulo(5,8))
